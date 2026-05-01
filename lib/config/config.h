e0b6b862f9d819235aadb3067b5aca0029613bec
Isaac Pawley <75295202+i-Saac-IV@users.noreply.github.com>
2026-05-01 21:41:45 +1200

    chore: added global config file

---
+++ lib/config/config.h
@@ -0,0 +1,39 @@
+/*
+
+File:   config.h
+Author: Isaac Pawley
+Date:   01-05-2026
+
+*/
+
+#ifndef INC_CONFIG_H_
+#define INC_CONFIG_H_
+
+#include <stdint.h>
+
+typedef enum {
+    TIME_FORMAT_12H = 0,
+    TIME_FORMAT_24H
+} time_format_t;
+
+typedef struct {
+    uint8_t display_mode;
+    uint8_t color_palette;
+    uint8_t nightMode_start;
+    uint8_t nightMode_end;
+    bool auto_sleep;
+    time_format_t time_format; 
+    bool enable_demo;
+} config_t;
+
+config_t config = {
+    .display_mode = 0,
+    .color_palette = 0,
+    .nightMode_start = 19,
+    .nightMode_end = 7,
+    .auto_sleep = 1,
+    .time_format = TIME_FORMAT_12H,
+    .enable_demo = 1
+};
+
+#endif /* INC_CONFIG_H_ */
