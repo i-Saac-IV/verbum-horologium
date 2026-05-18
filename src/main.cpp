90a7127e1935f1288e9130256c155cf90614821c
Isaac Pawley <75295202+i-Saac-IV@users.noreply.github.com>
2026-05-19 09:23:49 +1200

    fix: added word clock into main code

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,7 @@ Repo:   https://github.com/i-Saac-IV/verbum-horologium
 #include "staircase_clock.h"
 #include "buttons.h"
 #include "config.h"
+#include "word_clock.h"
 
 void setup() {
     task_scheduler_init();
@@ -32,16 +33,26 @@ void loop() {
     task_scheduler_executeEnabledTasks();
     display_fill(CRGB::Black);
     
-    if (buttons_getEvent(BUTTON_LEFT) == BUTTON_EVENT_SHORT_PRESS) {
+    if (buttons_getEvent(BTN_LEFT) == BTN_EVENT_SHORT_PRESS) {
         config.display_mode--;
-    } else if (buttons_getEvent(BUTTON_RIGHT) == BUTTON_EVENT_SHORT_PRESS) {
+    } else if (buttons_getEvent(BTN_RIGHT) == BTN_EVENT_SHORT_PRESS) {
         config.display_mode++;
+    }
+    
+    if (buttons_getPressed(BTN_TOP) == BTN_EVENT_DOWN) {
+        display_fill(CRGB::Purple);
+    }
+
+    if (buttons_getEvent(BTN_TOP) == BTN_EVENT_SHORT_PRESS) {
+        display_fill(CRGB::Red);
     }
 
     if (config.display_mode == 0) {
         staircase_clock_displayTime();
     } else if (config.display_mode == 1) {
         digital_clock_displayTime();
+    } else if (config.display_mode == 2) {
+        word_clock_displayTime();
     } else {
         // do nothing.
     }
