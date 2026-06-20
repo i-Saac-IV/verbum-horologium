fc199084cc254d6a026b206165dedbe0d5a87bb5
Isaac Pawley <75295202+i-Saac-IV@users.noreply.github.com>
2026-06-20 16:23:35 +1200

    fix: changed time return type

--- lib/serial/serial.cpp
+++ lib/serial/serial.cpp
@@ -18,8 +18,8 @@ void serial_printf(const char *format, ...);
 void serial_taskPrint() {
     serial_printf("Front: %d\tDisplay: %d\tPCB: %d\n", daylight_sensor_getRawBrightness(FRONT_SENSOR), daylight_sensor_getRawBrightness(DISPLAY_SENSOR), daylight_sensor_getRawBrightness(PCB_SENSOR));
     serial_printf("Front: %d\tDisplay: %d\tPCB: %d\n", daylight_sensor_getScaledBrightness(FRONT_SENSOR), daylight_sensor_getScaledBrightness(DISPLAY_SENSOR), daylight_sensor_getScaledBrightness(PCB_SENSOR));
-    DateTime now = rtc_getTime();
-    serial_printf("Time: %02u:%02u\n", now.hour(), now.minute());
+    DateTime* now = rtc_getTime();
+    serial_printf("Time: %02u:%02u\n", now->hour(), now->minute());
     serial_printf("\n");
 }
 
