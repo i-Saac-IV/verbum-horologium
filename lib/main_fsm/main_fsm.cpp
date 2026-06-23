a811ee6101a3a1e9f6c7dfe9d6e8170eb9bde8e8
Isaac Pawley <75295202+i-Saac-IV@users.noreply.github.com>
2026-06-23 14:07:18 +1200

    feat: added setting of the time to fsm

--- lib/main_fsm/main_fsm.cpp
+++ lib/main_fsm/main_fsm.cpp
@@ -1,10 +1,13 @@
 /*
-    File:   main_fsm.cpp
-    Author: Isaac Pawley
-    Date:   20-05-2026
+
+File:   main_fsm.cpp
+Author: Isaac Pawley
+Date:   20-05-2026
+
 */
 
 #include "main_fsm.h"
+#include "rtc.h"
 #include <Arduino.h>
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
@@ -30,6 +33,10 @@ AppState_t* app_get(void) {
 
 #include "config.h"
 
+static uint8_t edit_hour;
+static uint8_t edit_minute;
+static bool time_dirty;
+
 Setting_t settings[] = {
     {(uint8_t*)&config.time_format,      0, 1, 1},
     {(uint8_t*)&config.min_brightness,   5, 255, 5},
@@ -39,7 +46,9 @@ Setting_t settings[] = {
     {(uint8_t*)&config.auto_sleep,       0, 1, 1},
     {(uint8_t*)&config.enable_demo,      0, 1, 1},
     {(uint8_t*)&config.transition_effect, 0, 1, 1},
-    {(uint8_t*)&config.color_mode, 0, 2, 1}
+    {(uint8_t*)&config.color_mode, 0, 2, 1},
+    {&edit_hour, 0, 23, 1},
+    {&edit_minute, 0, 59, 1 }
 };
 
 const uint8_t SETTINGS_COUNT = sizeof(settings) / sizeof(settings[0]);
@@ -75,8 +84,7 @@ void action_enter_settings(AppState_t* app);
 void action_settings_next(AppState_t* app);
 void action_settings_prev(AppState_t* app);
 void action_settings_begin_edit(AppState_t* app);
-void action_settings_save(AppState_t* app);
-void action_settings_cancel(AppState_t* app);
+void action_settings_stop_edit(AppState_t* app);
 void action_settings_increment(AppState_t* app);
 void action_settings_decrement(AppState_t* app);
 void action_exit_settings(AppState_t* app);
@@ -190,13 +198,15 @@ static const Transition_t settings_table[] = {
     { SETTINGS_EDIT_MODE,
       {INPUT_EVENT_SHORT_PRESS, INPUT_SOURCE_BUTTON_TOP},
       SETTINGS_IDLE_MODE,
-      nullptr },
+      action_settings_stop_edit },
 
     { SETTINGS_EDIT_MODE,
       {INPUT_EVENT_LONG_PRESS, INPUT_SOURCE_BUTTON_TOP},
       SETTINGS_IDLE_MODE,
-      nullptr },
+      action_settings_stop_edit },
 };
+
+static uint8_t settings_original_value;
 
 // =====================================================
 // INIT
@@ -278,6 +288,13 @@ void action_change_color_pallette(AppState_t* app) {
 
 void action_enter_settings(AppState_t* app) {
     app->settings_mode = SETTINGS_IDLE;
+
+    DateTime* now = rtc_getTime();
+
+    edit_hour = now->hour();
+    edit_minute = now->minute();
+
+    time_dirty = false;
 }
 
 // =====================================================
@@ -293,7 +310,24 @@ void action_settings_prev(AppState_t* app) {
 }
 
 void action_settings_begin_edit(AppState_t* app) {
-    // Not much here...
+    DateTime* now = rtc_getTime();
+
+    edit_hour = now->hour();
+    edit_minute = now->minute();
+
+    time_dirty = false;
+}
+
+void action_settings_stop_edit(AppState_t* app) {
+    Setting_t* s = &settings[(uint8_t)app->settings_screen];
+
+    if (*s->value == settings_original_value)
+        return;
+
+    if (time_dirty) {
+        rtc_setTime(edit_hour, edit_minute);
+        time_dirty = false;
+    }
 }
 
 void action_settings_increment(AppState_t* app) {
@@ -307,6 +341,10 @@ void action_settings_increment(AppState_t* app) {
     } else {
         *s->value = s->min;
     }
+
+    if (app->settings_screen == SETTINGS_SCREEN_SET_HOURS || app->settings_screen == SETTINGS_SCREEN_SET_MINUTES) {
+        time_dirty = true;
+    }
 }
 
 void action_settings_decrement(AppState_t* app) {
@@ -319,6 +357,10 @@ void action_settings_decrement(AppState_t* app) {
         }
     } else {
         *s->value = s->max;
+    }
+
+    if (app->settings_screen == SETTINGS_SCREEN_SET_HOURS || app->settings_screen == SETTINGS_SCREEN_SET_MINUTES) {
+        time_dirty = true;
     }
 }
 
